511c275e5f40be3f87ee01f34f41c95d1fcf5032
Alex Krentsel <akrentsel@users.noreply.github.com>
2019-06-28 12:22:20 +0300

    Update geolocate.rb

--- geolocate.rb
+++ geolocate.rb
@@ -1,9 +1,9 @@
 class Geolocate < Formula
   desc "A command line tool to find the geographic location of an IP or URL."
   homepage "https://github.com/akrentsel/geolocate"
-  url "https://github.com/akrentsel/geolocate/raw/master/archive/geolocate-1.0.0.tar.gz"
-  sha256 "d93e350be02d3c3b8543b905ea5d575295c0190e0c358b814654935e884a7b20"
-  version "1.0.0"
+  url "https://github.com/akrentsel/geolocate/raw/master/archive/geolocate-1.0.1.tar.gz"
+  sha256 "28b2b56f57193edee4e2d32b80a3e2935ec9f3970cc3b422ed9b7b117cd3afcb"
+  version "1.0.1"
 
   depends_on "curl"
 
